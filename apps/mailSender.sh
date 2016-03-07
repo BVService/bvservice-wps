d0f0ca9f9155908a3d6773e7370a5a5f2704434b
Mounirsky <kaizersozy@gmail.com>
2016-03-08 00:10:38 +0100

    update mailSender & WMC generator

--- apps/mailSender.sh
+++ apps/mailSender.sh
@@ -21,27 +21,29 @@ Content-Type: text/html; charset=utf-8
 Content-Transfer-Encoding: 7bit
 
 <html>
-<body><img src='cid:image1' align='right'/><div align='left'>
 
-<h2> Résultats de la simulation OpenFLUID </h2>
+<body><img src='cid:image1' align='right' />
+    <div align='left'>
+        <h2> Résultats de la simulation OpenFLUID </h2>
+        <br></br>
+        <p>Le traitement et terminé avec succès ! Temps de traitement et de $5 sec, ( ~ $6 min)</p>
+        <br></br>
+        <p>Pour visualiser la carte sur un visualiseur avancé <a style="font-weight:bold;" href="http://bvservice.fr/mapfishapp/?wmc=http://bvservice.fr/context/wmc/$3.wmc">Cliquez ici</a> </p>
+        <p>Pour visualiser sur un visualiseur mobile <a style="font-weight:bold;" href="http://geoxxx.agrocampus-ouest.fr/mviewer/?wmc=http://bvservice.fr/context/wmc/$3.wmc">Cliquez ici</a> ou <a style="font-weight:bold;" href="http://geoxxx.agrocampus-ouest.fr/sviewer/?wmc=http://bvservice.fr/context/wmc/$3.wmc"> ici </a> </p>
+        <br></br>
+        <p>Pour télécharger le ShapeFile de la couche n° 1 <a style="font-weight:bold;" href="http://bvservice.fr/outputs/$2/wfs?service=WFS&version=1.0.0&request=GetFeature&typename=$3&outputFormat=SHAPE-ZIP">Cliquez ici</a></p>
+        <p>Pour télécharger le ShapeFile de la couche n° 2 <a style="font-weight:bold;" href="http://bvservice.fr/outputs/$2/wfs?service=WFS&version=1.0.0&request=GetFeature&typename=$4&outputFormat=SHAPE-ZIP">Cliquez ici</a></p>
+        <br></br>
+        <p>Pour télécharger le KML (Google Earth) de la couche n° 1 <a style="font-weight:bold;" href="http://bvservice.fr/outputs/$2/wfs?service=WFS&version=1.0.0&request=GetFeature&typename=$3&outputFormat=KML">Cliquez ici</a></p>
+        <p>Pour télécharger le KML (Google Earth) de la couche n° 2 <a style="font-weight:bold;" href="http://bvservice.fr/outputs/$2/wfs?service=WFS&version=1.0.0&request=GetFeature&typename=$4&outputFormat=KML">Cliquez ici</a></p>
+        <br></br>
+        <p>-----------------</p>
 
-<p>Le taitement et terminé et votre couche a été publiée avec succé !</p>
+        <br></br>
+        <p>Message généré automatiquement. Ne pas répondre à ce message !</p>
+    </div>
+</body>
 
-<p>Pour visualiser sur un visualiseur avancé <a href ="http://bvservice.fr/mapfishapp/?wmc=http://bvservice.fr/context/wmc/$3.wmc" >Cliquez ici</a> </p>
-
-<p>Pour visualiser sur un visualiseur mobile <a href ="http://geoxxx.agrocampus-ouest.fr/mviewer/?wmc=http://bvservice.fr/context/wmc/$3.wmc" >Cliquez ici</a> ou <a href ="http://geoxxx.agrocampus-ouest.fr/sviewer/?wmc=http://bvservice.fr/context/wmc/$3.wmc" >ici</a> </p>
-
-<p>Pour télécharger le ShapeFile de la couche n° 1 <a href ="http://bvservice.fr/outputs/$2/wfs?service=WFS&version=1.0.0&request=GetFeature&typename=$3&outputFormat=SHAPE-ZIP" >Cliquez ici</a></p>
-
-<p>Pour télécharger le ShapeFile de la couche n° 2 <a href ="http://bvservice.fr/outputs/$2/wfs?service=WFS&version=1.0.0&request=GetFeature&typename=$4&outputFormat=SHAPE-ZIP" >Cliquez ici</a></p>
-
-<p>Pour télécharger le KML (Google Earth) de la couche n° 1 <a href ="http://bvservice.fr/outputs/$2/wms/kml?layers=$3" >Cliquez ici</a></p>
-
-<p>Pour télécharger le KML (Google Earth) de la couche n° 2 <a href ="http://bvservice.fr/outputs/$2/wms/kml?layers=$4" >Cliquez ici</a></p>
-
-<p>Le temps de traitement et de $5 sec, ( ~ $6 min)</p>
-
-</div></body>
 </html>
 
 --XYZ
